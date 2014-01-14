09197fae3e5c3fbdb395491b878e3e3eecd1e751
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-01-14 04:13:43 +0000

    add study objective c xcode project

---
+++ trunk/mac/objective-c/xcode/StudyObjectiveC/StudyObjectiveC/SClass.m
@@ -0,0 +1,13 @@
+//
+//  SClass.m
+//  StudyObjectiveC
+//
+//  Created by Shi, Zexing on 1/13/14.
+//  Copyright (c) 2014 zexing. All rights reserved.
+//
+
+#import "SClass.h"
+
+@implementation SClass
+
+@end
---
+++ trunk/mac/objective-c/xcode/StudyObjectiveC/StudyObjectiveC/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  StudyObjectiveC
+//
+//  Created by Shi, Zexing on 1/13/14.
+//  Copyright (c) 2014 zexing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ANSI-C.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
