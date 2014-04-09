920d3c6b444c7e2e20adfc57ee426958e1b304d7
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-04-09 08:18:23 +0000

    download all houses in a zip

--- trunk/python/RealEstateFilter/realestatefilter.py
+++ trunk/python/RealEstateFilter/realestatefilter.py
@@ -7,39 +7,55 @@ class RealEstateFilter(object):
 	def __init__(self, arg):
 		super(RealEstateFilter, self).__init__()
 		self.arg = arg
-		#print "arg.len = " + str(len(arg)) 
-		#print 'args : ' + ', '.join(arg)
 		self.zips = []
+		self.houses = []
 		for i in xrange(1,len(arg)):
 			self.zips.append(arg[i])
 
+	def retrieveUrl(self, url):
+		f = urllib2.urlopen(url)
+		data = f.read()
+		f.close()
+		return data
+
+	def collectHouses(self, data):
+		idx = data.find('<article')
+		while idx != -1:
+			idx2 = data.find('</article>', idx) + len('</article>')
+			self.houses.append(data[idx:idx2])
+			idx = data.find('<article', idx2)
+
+	''' query houses by zip and continue to page $page '''
+	def queryByZipMore(self, zip, page):
+		url = 'http://www.zillow.com/homes/' + zip + '_rb/' + page + '_p/'
+		data = self.retrieveUrl(url)
+		self.collectHouses(data)
+
+	''' query houses by zip '''
 	def queryByZip(self, zip):
 		url = 'http://www.zillow.com/homes/' + zip + '_rb/'
-		#print 'url = ' + url
-		url = 'http://www.zillow.com/search/RealEstateSearch.htm?citystatezip=' + zip
+		#url = 'http://www.zillow.com/search/RealEstateSearch.htm?citystatezip=' + zip
 		#r = urllib2.Request(url)
 		#r.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
 		#r.add_header('Referer', 'http://www.zillow.com/')
-		f = urllib.urlopen(url)
-		#f = urllib2.urlopen(r)
-		data = f.read()
-		f.close()
-		print data
+		data = self.retrieveUrl(url)
+		#print data
 		idx = data.find('<div id="search-results"')
 		#print data[idx:]
-		houses = []
-		i = 0
+		self.collectHouses(data)
+		#print 'houses.length = ' + str(len(self.houses))
+		idx = data.find('changePage(')
+		pages = []
 		while idx != -1:
-			idx = data.find('<article', idx)
-			if idx == -1:
-				break
-			idx2 = data.find('</article>', idx) + len('</article>')
-			houses.append(data[idx:idx2])
-			i = i + 1
-			#print 'i = ' + str(i)
-			idx = idx2
-		print len(houses)
-		print houses[0]
+			idx2 = data.find(')', idx)
+			page = data[idx + len('changePage('):idx2]
+			if page not in pages:
+				pages.append(page)
+				self.queryByZipMore(zip, page)
+			idx = data.find('changePage(', idx2)
+		print 'houses.length = ' + str(len(self.houses))
+		for house in self.houses:
+			print house
 		#print data.count('<article')
 		#print data.count('</article>')
 		#print data
