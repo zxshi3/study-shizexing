a7f9701a28a04f22bf3bc8db50e32c92020d1ce9
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-04-18 07:51:02 +0000

    query school info

--- python/RealEstateFilter/realestatefilter.py
+++ python/RealEstateFilter/realestatefilter.py
@@ -18,56 +18,10 @@ class RealEstateFilter(object):
 		for i in xrange(1,len(arg)):
 			self.zips.append(arg[i])
 
-	def parseHouseInfo(self, house):
+	def parseRealEstateInfo(self, house):
 		estate = RealEstate()
 		print '======================================'
-		# 1. get id
-		idx = house.find('zpid_') + len('zpid_')
-		idx2 = house.find('"', idx)
-		zpid = house[idx:idx2]
-		estate.id = zpid
-		print 'zillow property id = ' + zpid
-		# 2. get lat, lon
-		idx = house.find('longitude="', idx2) + len('longitude="')
-		idx2 = house.find('"', idx)
-		longitude = house[idx:idx2]
-		estate.lon = longitude
-		print 'longitude = ' + longitude
-		idx = house.find('latitude="', idx2) + len('latitude="')
-		idx2 = house.find('"', idx)
-		latitude = house[idx:idx2]
-		estate.lat = latitude
-		print 'latitude = ' + latitude
-		# 3. get url
-		idx = house.find('/homedetails')
-		idx2 = house.find('"', idx)
-		url = house[idx:idx2]
-		print 'url = ' + url
-		estate.url = url
-		# 4,5 type & property type
-		idx = house.find('<dl class="property-info-list col-1 column">')
-		idx = house.find('<strong>', idx) + len('<strong>')
-		idx2 = house.find('</strong>', idx)
-		houseType = house[idx:idx2]
-		if houseType.count('<span') > 0:
-			idx = houseType.find('<span')
-			idx = houseType.find('>', idx) + len('>')
-			idx2 = houseType.find('</span>')
-			houseType = houseType[idx:idx2]
-		print 'house type : ' + houseType
-		estate.setType(houseType)
-		#print estate.type
-		#print estate.propertytype
-		# TODO parse info by housetype
-		'''
-		{
-			'House For Sale' : lambda e, h : e.parseHouse(h)
-		}[houseType](estate, house)
-		'''
-		{
-			'House For Sale' : lambda e, h : e.parseHouse(h)
-		}.get(houseType, lambda e, h : 'DO NOTHING')(estate, house)
-
+		estate.parseRealEstateInfo(house)
 		self.estates.append(estate)
 
 	def collectHouses(self, data):
@@ -76,7 +30,7 @@ class RealEstateFilter(object):
 			idx2 = data.find('</article>', idx) + len('</article>')
 			house = data[idx:idx2]
 			#print house
-			self.parseHouseInfo(house)
+			self.parseRealEstateInfo(house)
 			self.houses.append(data[idx:idx2])
 			idx = data.find('<article', idx2)
 
