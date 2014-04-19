dd714571a7024ed84ec31e415342a00cf661555b
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-04-19 09:20:32 +0000

    add filter

--- trunk/python/RealEstateFilter/realestatefilter.py
+++ trunk/python/RealEstateFilter/realestatefilter.py
@@ -4,7 +4,6 @@ import sys, urllib, urllib2
 #import realestate
 from realestate import RealEstate
 from httpreader import HttpReader
-#from realestate import Address
 
 class RealEstateFilter(object):
 	"""docstring for RealEstateFilter"""
@@ -12,16 +11,46 @@ class RealEstateFilter(object):
 		super(RealEstateFilter, self).__init__()
 		self.arg = arg
 		self.zips = []
-		# original house information
-		self.houses = []
+		# all estates
 		self.estates = []
+		# filtered estates
+		self.matchedEstates = []
 		for i in xrange(1,len(arg)):
 			self.zips.append(arg[i])
 
-	def parseRealEstateInfo(self, house):
-		estate = RealEstate()
+	def query(self):
+		for zip in self.zips:
+			self.__queryByZip(zip)
+
+	def filter(self):
+		for estate in self.estates:
+			# 1. price
+			if estate.price > 700000:
+				#print '>70w. price = ' + str(estate.price) + '\turl = ' + estate.url
+				continue
+			# 2. bedroom
+			if estate.bedroom < 3:
+				#print '<3b. bedroom = ' + str(estate.bedroom) + '\turl = ' + estate.url
+				continue
+			#finally, we find match estate
+			# 3. school
+			n = len(estate.schools)
+			ratings = 0
+			for school in estate.schools:
+				ratings += school.rating
+			avg = ratings / n
+			if avg < 7:
+				#print 'rating < 7. avg = ' + str(avg) + '\turl = ' + estate.url
+				continue
+			self.matchedEstates.append(estate)
+
+	# @deprecated
+	def parseRealEstateInfo(self, house_html):
+		estate = RealEstate(house_html)
 		print '======================================'
-		estate.parseRealEstateInfo(house)
+		estate.parseRealEstateInfo(house_html)
+		self.estates.append(estate)
+		''' move to filter step
 		if estate.price > 700000:
 			#print 'tooooooooooooooooexpensive'
 			#print 'estate:\n' + str(estate)
@@ -29,25 +58,26 @@ class RealEstateFilter(object):
 		else:
 			print 'estate:\n' + str(estate)
 			self.estates.append(estate)
+		'''
 
-	def collectHouses(self, data):
+	def __collectEstates(self, data):
 		idx = data.find('<article')
 		while idx != -1:
 			idx2 = data.find('</article>', idx) + len('</article>')
-			house = data[idx:idx2]
-			#print house
-			self.parseRealEstateInfo(house)
-			self.houses.append(data[idx:idx2])
+			house_html = data[idx:idx2]
+			#print house_html
+			estate = RealEstate(house_html)
+			self.estates.append(estate)
 			idx = data.find('<article', idx2)
 
-	''' query houses by zip and continue to page $page '''
-	def queryByZipMore(self, zip, page):
+	''' query estates by zip and continue to page $page '''
+	def __queryByZipMore(self, zip, page):
 		url = 'http://www.zillow.com/homes/' + zip + '_rb/' + page + '_p/'
 		data = HttpReader.retrieveUrl(url)
-		self.collectHouses(data)
+		self.__collectEstates(data)
 
-	''' query houses by zip '''
-	def queryByZip(self, zip):
+	''' query estates by zip '''
+	def __queryByZip(self, zip):
 		url = 'http://www.zillow.com/homes/' + zip + '_rb/'
 		#url = 'http://www.zillow.com/search/RealEstateSearch.htm?citystatezip=' + zip
 		#r = urllib2.Request(url)
@@ -57,8 +87,7 @@ class RealEstateFilter(object):
 		#print data
 		idx = data.find('<div id="search-results"')
 		#print data[idx:]
-		self.collectHouses(data)
-		#print 'houses.length = ' + str(len(self.houses))
+		self.__collectEstates(data)
 		idx = data.find('changePage(')
 		pages = []
 		while idx != -1:
@@ -66,23 +95,16 @@ class RealEstateFilter(object):
 			page = data[idx + len('changePage('):idx2]
 			if page not in pages:
 				pages.append(page)
-				self.queryByZipMore(zip, page)
+				self.__queryByZipMore(zip, page)
 			idx = data.find('changePage(', idx2)
-		#print 'houses.length = ' + str(len(self.houses))
-		#self.parseHouseInfo(self.houses[0])
-		#for house in self.houses:
-		#	print house
-		#print data.count('<article')
-		#print data.count('</article>')
-		#print data
-		#<div id="search-results"
-		#<article
-		#</article>
-
-	def query(self):
-		for zip in self.zips:
-			self.queryByZip(zip)
 
 if __name__ == '__main__':
 	filter = RealEstateFilter(sys.argv)
 	filter.query()
+	#print '<search-results>'
+	#print filter.estates
+	#print '</search-results>'
+	filter.filter()
+	print '<match-result>'
+	print filter.matchedEstates
+	print '</match-result>'
