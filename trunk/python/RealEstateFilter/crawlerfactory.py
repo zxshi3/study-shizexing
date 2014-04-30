31959169396401bec0dade80b7aa89de348f0fa8
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-04-30 05:12:26 +0000

    refactor, add crawlerfactory

---
+++ trunk/python/RealEstateFilter/crawlerfactory.py
@@ -0,0 +1,14 @@
+from redfincrawler import RedfinCrawler
+from zillowcrawler import ZillowCrawler
+
+class CrawlerFactory(object):
+	"""docstring for CrawlerFactory"""
+	def __init__(self):
+		super(CrawlerFactory, self).__init__()
+	
+	@classmethod
+	def getCrawler(cls, url):
+		if url.count('zillow') > 0:
+			return ZillowCrawler()
+		elif url.count('redfin') > 0:
+			return RedfinCrawler()
