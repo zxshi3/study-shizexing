3820024c90ad6b2bcbf0572c7b448400d981cda2
shizexing@gmail.com <shizexing@gmail.com@9b14c1aa-5174-8333-eb4d-48037f09d8f2>
2014-05-06 00:57:18 +0000

    modify json test

---
+++ cjson/main.cpp
@@ -0,0 +1,80 @@
+/**
+ * this example show you how to use cJSON to parse json file
+ *
+ * In the test.json, it shows a simple json object
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "cJSON.h"
+#include <string>
+
+static const char filename[] = "test.json";
+
+static std::string loadJson(const std::string & filename) {
+	FILE* fp = fopen(filename.c_str(), "r");
+	if (fp)
+	{
+		long size;
+		char * buf;
+		// read file data
+		fseek(fp, 0, SEEK_END);
+		size = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		buf = (char*)malloc((size_t)size + 1);
+		assert(buf);
+		memset(buf, 0, size + 1);
+		fread(buf, sizeof(char), (size_t)size, fp);
+		buf[size] = '\0';
+		fclose(fp);
+        std::string data = std::string(buf, size);
+    	free(buf);
+    	buf = 0;
+        return data;
+	}
+	else
+	{
+		fprintf(stderr, "can not open %s\n", filename.c_str());
+        return std::string();
+	}	
+}
+
+int main(int argc, char** argv)
+{
+	cJSON * tmp;
+	cJSON * json;
+	
+    std::string data = loadJson(filename);
+	// parse json file
+	json = cJSON_Parse(data.c_str());
+	//cJSON * tmp;
+	tmp = cJSON_GetObjectItem(json, "string");
+	if (tmp)
+	{
+		char* str = tmp->valuestring;
+		printf("string : %s\n", str);
+	}
+	tmp = cJSON_GetObjectItem(json, "float");
+	if (tmp)
+	{
+		double f = tmp->valuedouble;
+		printf("float value : %f\n", f);
+	}
+	tmp = cJSON_GetObjectItem(json, "int");
+	if (tmp)
+	{
+		int i = tmp->valueint;
+		printf("int value : %d\n", i);
+	}
+	tmp = cJSON_GetObjectItem(json, "boolean");
+	if (tmp)
+	{
+		int b = tmp->valueint;
+		printf("boolean value : %s\n", (1 == b) ? "true" : "false");
+	}
+
+	cJSON_Delete(json);
+
+	return 0;
+}
